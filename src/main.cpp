7cc127678ef3d4ffb13cd1208878d67e49a38185
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2023-06-12 19:15:27 +0300

    Update example to use new API

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,15 @@
 
 #define BLYNK_FIRMWARE_VERSION        "0.1.0"
 
-#define BLYNK_PRINT Serial
+// Debug output
+#define BLYNK_PRINT                   Serial
+
+// Redefine NCP type and connection port settings, if needed
+//#define BLYNK_NCP_SERIAL              Serial1
+//#define BLYNK_NCP_BAUD                2000000
+//#define BLYNK_NCP_MACCHINA_SUPERB
 
 #include <BlynkNcpClient.h>
-#include <ExampleHelpers.h>
 
 BlynkTimer timer;
 
@@ -20,36 +25,32 @@ BLYNK_DISCONNECTED() {
   BLYNK_LOG("Blynk disconnected");
 }
 
-BLYNK_WRITE(V1) {
-  int value = BlynkMathClamp(param.asInt(), 0, 63);
-
-  char buff[64];
-  memset(buff, '|', sizeof(buff));
-  if (value) {
-    buff[value] = '\0';
-  } else {
-    strcpy(buff, " ");
-  }
-
-  BLYNK_LOG("Got: %s", buff);
-
-  Blynk.virtualWrite(V2, buff);
-  Blynk.setProperty(V2, "color", valueToColor(value));
+template <typename T>
+void waitSerialConsole(T& ser) {
+  // Wait for serial console, up to 3 seconds
+  const uint32_t tstart = millis();
+  while (!ser && (millis() - tstart < 2900)) { delay(1); }
+  delay(100);
 }
 
 void setup() {
   Serial.begin(115200);
-  waitSerialConsole(Serial);
   Serial.println();
+  waitSerialConsole(Serial);
+
+  if (Blynk.initNCP()) {
+    String ver = Blynk.getNcpVersion();
+    BLYNK_LOG("Blynk.NCP firmware: %s", ver.c_str());
+  } else {
+    BLYNK_LOG("Cannot communicate to Blynk.NCP");
+    return;
+  }
 
   Blynk.onStateChange([]() {
     BLYNK_LOG("State: %s", Blynk.getStateString());
   });
 
-  if (!Blynk.begin(BLYNK_TEMPLATE_ID, BLYNK_TEMPLATE_NAME)) {
-    BLYNK_LOG("Blynk.NCP initialization failed!");
-    while (true) { delay(100); }
-  }
+  Blynk.begin(BLYNK_TEMPLATE_ID, BLYNK_TEMPLATE_NAME);
 
   timer.setInterval(1000, []() {
     Blynk.virtualWrite(V0, millis());
