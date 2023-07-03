db6f1bb84bcd44464c33e69e59e4b458b6dd68ca
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2023-07-03 23:14:40 +0300

    Update ota.bin generation

--- tools/extra_ota_bin.py
+++ tools/extra_ota_bin.py
@@ -4,21 +4,24 @@ import os
 import shutil
 
 pioenv = env["PIOENV"]
+ota_bin = f"build/firmware_{pioenv}.ota.bin"
 
 os.makedirs("./build", exist_ok=True)
 
 def copy_bin(source, target, env):
-    program_path = target[0].get_path()
-    shutil.copy(program_path, f"build/firmware_{pioenv}.ota.bin")
+    shutil.copy(target[0].get_path(), ota_bin)
 
 if pioenv in ["rp2040connect"]:
     # BIN from ELF
     env.AddPostAction(
         "$BUILD_DIR/${PROGNAME}.elf",
         env.VerboseAction(" ".join([
-            "$OBJCOPY", "-O", "binary", "$BUILD_DIR/${PROGNAME}.elf", "$BUILD_DIR/${PROGNAME}.bin"
-        ]), "Building $BUILD_DIR/${PROGNAME}.bin")
+            "$OBJCOPY", "-O", "binary", "\"$BUILD_DIR/${PROGNAME}.elf\"", ota_bin
+        ]), f"Building {ota_bin}")
+    )
+elif not "mbed" in pioenv:
+    env.AddPostAction(
+        "$BUILD_DIR/${PROGNAME}.bin",
+        env.VerboseAction(copy_bin, f"Building {ota_bin}")
     )
 
-if not "mbed" in pioenv:
-    env.AddPostAction("$BUILD_DIR/${PROGNAME}.bin", copy_bin)
