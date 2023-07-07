7879340b14b6783edef0046958b8d9af9d6bebf2
Volodymyr Shymanskyy <vshymanskyi@gmail.com>
2023-07-07 17:44:41 +0300

    Cleanup

--- src/ArduinoUtils.h
+++ src/ArduinoUtils.h
@@ -1,15 +1,20 @@
 
 template <typename T>
 void waitSerialConsole(T& ser) {
+#if !defined(LINUX)
   // Wait for serial console, up to 3 seconds
   const uint32_t tstart = millis();
   while (!ser && (millis() - tstart < 2900)) { delay(1); }
   delay(100);
+#endif
 }
 
 #if defined(LINUX)
 int main(int argc, char* argv[])
 {
+  (void) argc;
+  (void) argv;
+
   setup();
   while (true) {
     loop();
